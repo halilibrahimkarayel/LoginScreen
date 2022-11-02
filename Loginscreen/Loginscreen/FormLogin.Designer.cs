a7ee50807a26ad4fa817e81228230e07b814607e
halilibrahimkarayel <132130016@ogr.uludag.edu.tr>
2022-11-02 19:45:52 +0300

    132130016 loginsreen ödev
    nyp ödev halil ibrahim karayel

---
+++ Loginscreen/Loginscreen/FormLogin.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Loginscreen
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelKullaniciAdi = new System.Windows.Forms.Label();
+            this.labelSifre = new System.Windows.Forms.Label();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.textBoxSifre = new System.Windows.Forms.TextBox();
+            this.buttonGiris = new System.Windows.Forms.Button();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelKullaniciAdi
+            // 
+            this.labelKullaniciAdi.AutoSize = true;
+            this.labelKullaniciAdi.Location = new System.Drawing.Point(238, 102);
+            this.labelKullaniciAdi.Name = "labelKullaniciAdi";
+            this.labelKullaniciAdi.Size = new System.Drawing.Size(70, 13);
+            this.labelKullaniciAdi.TabIndex = 0;
+            this.labelKullaniciAdi.Text = "Kullanıcı Adı :";
+            // 
+            // labelSifre
+            // 
+            this.labelSifre.AutoSize = true;
+            this.labelSifre.Location = new System.Drawing.Point(274, 174);
+            this.labelSifre.Name = "labelSifre";
+            this.labelSifre.Size = new System.Drawing.Size(34, 13);
+            this.labelSifre.TabIndex = 1;
+            this.labelSifre.Text = "Şifre :";
+            // 
+            // textBoxKullaniciAdi
+            // 
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(314, 99);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxKullaniciAdi.TabIndex = 2;
+            // 
+            // textBoxSifre
+            // 
+            this.textBoxSifre.Location = new System.Drawing.Point(314, 171);
+            this.textBoxSifre.Name = "textBoxSifre";
+            this.textBoxSifre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSifre.TabIndex = 3;
+            // 
+            // buttonGiris
+            // 
+            this.buttonGiris.Location = new System.Drawing.Point(200, 321);
+            this.buttonGiris.Name = "buttonGiris";
+            this.buttonGiris.Size = new System.Drawing.Size(108, 68);
+            this.buttonGiris.TabIndex = 4;
+            this.buttonGiris.Text = "Giriş Yap";
+            this.buttonGiris.UseVisualStyleBackColor = true;
+            this.buttonGiris.Click += new System.EventHandler(this.buttonGiris_Click);
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.Location = new System.Drawing.Point(314, 320);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(113, 69);
+            this.buttonTemizle.TabIndex = 5;
+            this.buttonTemizle.Text = "Temizle";
+            this.buttonTemizle.UseVisualStyleBackColor = true;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 450);
+            this.Controls.Add(this.buttonTemizle);
+            this.Controls.Add(this.buttonGiris);
+            this.Controls.Add(this.textBoxSifre);
+            this.Controls.Add(this.textBoxKullaniciAdi);
+            this.Controls.Add(this.labelSifre);
+            this.Controls.Add(this.labelKullaniciAdi);
+            this.Name = "FormLogin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelKullaniciAdi;
+        private System.Windows.Forms.Label labelSifre;
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
+        private System.Windows.Forms.TextBox textBoxSifre;
+        private System.Windows.Forms.Button buttonGiris;
+        private System.Windows.Forms.Button buttonTemizle;
+    }
+}
+
---
+++ Loginscreen/Loginscreen/FormLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loginscreen
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGiris_Click(object sender, EventArgs e)
+        {
+            string kullaniciadi = textBoxKullaniciAdi.Text;
+            string sifre = textBoxSifre.Text;
+
+
+            if(kullaniciadi=="asd" && sifre=="asd")
+            {
+                Form1 f1 = new Form1();
+                f1.ShowDialog();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre hatalıdır.");
+                textBoxKullaniciAdi.Text = "";
+                textBoxSifre.Text = "";
+                    
+               
+            }
+        }
+
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            textBoxKullaniciAdi.Text = "";
+            textBoxSifre.Text = "";
+        }
+    }
+}
